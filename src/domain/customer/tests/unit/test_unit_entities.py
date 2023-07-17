65ee1a08c7012d9d05f0bf155396f039c27da966
candidosouza <candidosouzza@gmail.com>
2023-07-17 12:05:01 -0300

    test: unit test validate in the customer entity

--- src/domain/customer/tests/unit/test_unit_entities.py
+++ src/domain/customer/tests/unit/test_unit_entities.py
@@ -1,5 +1,6 @@
-from dataclasses import is_dataclass
 import unittest
+from unittest.mock import patch
+from dataclasses import FrozenInstanceError, is_dataclass
 from src.domain.customer.entities import Customer
 
 
@@ -7,3 +8,43 @@ class TestCustomerUnit(unittest.TestCase):
 
     def test_if_is_dataclass(self):
         self.assertTrue(is_dataclass(Customer))
+
+    def test_constructor(self):
+        with patch.object(Customer, 'validate') as mock_validate:
+            customer = Customer(
+                name='John Doe',
+                email='jhondoe@rmail.com',
+            )
+            mock_validate.assert_called_once()
+            self.assertEqual(customer.name, 'John Doe')
+            self.assertEqual(customer.email, 'jhondoe@rmail.com')
+            self.assertEqual(customer.id, customer.unique_entity_id.id)
+            self.assertEqual(customer.is_active, True)
+
+            customer = Customer(
+                name='John Doe',
+                email='jhondoe@rmail.com',
+                is_active=False,
+            )
+            self.assertEqual(customer.name, 'John Doe')
+            self.assertEqual(customer.email, 'jhondoe@rmail.com')
+            self.assertEqual(customer.id, customer.unique_entity_id.id)
+            self.assertEqual(customer.is_active, False)
+
+    def test_is_immutable(self):
+        with patch.object(Customer, 'validate'):
+            with self.assertRaises(FrozenInstanceError):
+                value_object = Customer(name='test', email='test@rmail.com')
+                value_object.name = 'fake id'
+
+    def test_activate(self):
+        with patch.object(Customer, 'validate'):
+            customer = Customer(name='test', email='test@rmail.com', is_active=False)
+            customer.activate()
+            self.assertTrue(customer.is_active)
+
+    def test_deactivate(self):
+        with patch.object(Customer, 'validate'):
+            customer = Customer(name='test', email='test@rmail.com')
+            customer.deactivate()
+            self.assertFalse(customer.is_active)
