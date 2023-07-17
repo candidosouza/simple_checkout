09db7f3ce122e6c7d598eb96bfaa027b96c1eae9
candidosouza <candidosouzza@gmail.com>
2023-07-17 11:50:35 -0300

    feat: add _set and to_dict method on entity interface

--- src/domain/shared/entities.py
+++ src/domain/shared/entities.py
@@ -1,5 +1,6 @@
 from abc import ABC
-from dataclasses import dataclass, field
+from dataclasses import asdict, dataclass, field
+from typing import Any
 from .value_objects import UniqueEntityId
 
 
@@ -13,3 +14,13 @@ class Entity(ABC):
     @property
     def id(self):
         return str(self.unique_entity_id)
+    
+    def _set(self, name: str, value: Any):
+        object.__setattr__(self, name, value)
+        return self
+    
+    def to_dict(self):
+        entity_dict = asdict(self)
+        entity_dict.pop('unique_entity_id')
+        entity_dict['id'] = self.id
+        return entity_dict
